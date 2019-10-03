69e79d1b6c7b643bd4b2218835bdcc0de1a78052
Alejandro <alecoexposito@gmail.com>
2019-10-03 17:41:28 -0400

    changes

--- bb-manager.sh
+++ bb-manager.sh
@@ -80,7 +80,7 @@ initialize_gps_flow() {
 	read -p "Entre el IMEI: " imei
 	sed -i 's:^[ \t]*DEVICE_IMEI[ \t]*=\([ \t]*.*\)$:DEVICE_IMEI='${imei}':' .env
 	read -p "Entre el ip del server donde está el tracker: " ip_tracker
-	sed -i 's:^[ \t]*DEVICE_IMEI[ \t]*=\([ \t]*.*\)$:DEVICE_IMEI='${ip_tracker}':' .env
+	sed -i 's:^[ \t]*TRACKER_IP[ \t]*=\([ \t]*.*\)$:TRACKER_IP='${ip_tracker}':' .env
 	echo "iniciando app bb"
 	pm2 start server.js
 	pm2 restart server
