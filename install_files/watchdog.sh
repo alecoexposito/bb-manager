1e5e2501dbfa23c6944e184d74388dd08ccdf522
Alejandro <alecoexposito@gmail.com>
2020-02-16 22:31:27 -0500

    cambiando el watchdog

--- install_files/watchdog.sh
+++ install_files/watchdog.sh
@@ -1,26 +1,41 @@
 #!/usr/bin/env bash
 
 date
-OUTPUT="$(qmicli -d /dev/cdc-wdm0 --nas-get-signal-strength)"
-echo "${OUTPUT}"
-STATUS="$(qmicli -d /dev/cdc-wdm0 --wds-get-packet-service-status)"
-echo "${STATUS}"
 
-if echo "$STATUS" | grep -q disconnected; then
-  echo "Modem is disconnected";
-  echo "Running ifdown wwan0"
+IP='69.64.32.172'
+if ping -c 1 $IP &> /dev/null
+then
+  echo "Conectado"
+else
+  echo "Desconectado, levantando la red de nuevo"
+  echo "Corriendo ifdown wwan0"
   /sbin/ifdown wwan0
   sleep 1
-  echo "Running ifup wwan0"
+  echo "Corriendo ifup wwan0"
   /sbin/ifup wwan0
 fi
 
+# OUTPUT="$(qmicli -d /dev/cdc-wdm0 --nas-get-signal-strength)"
+# echo "${OUTPUT}"
+# STATUS="$(qmicli -d /dev/cdc-wdm0 --wds-get-packet-service-status)"
+# echo "${STATUS}"
 
-PARAMS="{\"status\":\"$STATUS\"}"
+# if echo "$STATUS" | grep -q disconnected; then
+#   echo "Modem is disconnected";
+#   echo "Running ifdown wwan0"
+#   /sbin/ifdown wwan0
+#   sleep 1
+#   echo "Running ifup wwan0"
+#   /sbin/ifup wwan0
+# fi
 
 
-curl -H 'Content-Type: application/json' -d "$PARAMS" http://189.213.227.211:3007/api/v1/modem-query &
 
-ping -I wwan0 www.google.com -c 4
+# PARAMS="{\"status\":\"$STATUS\"}"
+
+
+# curl -H 'Content-Type: application/json' -d "$PARAMS" http://189.213.227.211:3007/api/v1/modem-query &
+
+# ping -I wwan0 www.google.com -c 4
 
 echo '******************************************************'
