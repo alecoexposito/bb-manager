c8a592c7f4c974d6c62528464032c1a2285a904d
Alejandro <alecoexposito@gmail.com>
2020-07-13 17:50:42 -0400

    sync-aws.py

---
+++ install_files/scripts/sync-aws.py
@@ -0,0 +1,29 @@
+import http.client
+import json
+import os
+
+# config = loadConfiguration()
+
+def loadConfiguration():
+    return []
+
+def getPath():
+    print("en el metodo")
+    conn = http.client.HTTPConnection('69.64.32.172/',  3010)
+    conn.request('GET', '/blackboxes/6/getPath')
+    resp = conn.getresponse()
+    print("resp: ", resp.read())
+    if resp.status == 200:
+        data = json.loads(resp.read())
+        print(data)
+        awsFolder = data['path']
+        return awsFolder
+    else:
+        return ''
+
+print("llamando  al metodo")
+path = getPath()
+
+# /usr/local/bin/aws s3 sync s3://zurikato-dev-01/1 /home/zurikato/tmp/1 --size-only --delete
+
+os.system("/usr/local/bin/aws s3 sync s3://" + path + " /home/zurikato/apps/tvz-media-server/media --size-only --delete")
