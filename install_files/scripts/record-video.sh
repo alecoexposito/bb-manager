b4f14f993d9044fa5b4c598164d64d2d53e9145c
Alejandro <alecoexposito@gmail.com>
2020-08-05 16:10:31 -0400

    increasing hls playlist size

--- install_files/scripts/record-video.sh
+++ install_files/scripts/record-video.sh
@@ -11,4 +11,4 @@ then
 fi
 
 
-/usr/bin/ffmpeg -rtsp_flags prefer_tcp -i "rtsp://$2:554/user=admin&password=&channel=1&stream=1.sdp" -acodec copy -vcodec copy -f hls -hls_time 30 -hls_list_size 3200 -hls_flags append_list+delete_segments -use_localtime 1 -hls_segment_filename "/home/zurikato/video-backup/$1/%Y-%m-%d_%H-%M-%S_hls.ts" /home/zurikato/video-backup/$1/playlist.m3u8
+/usr/bin/ffmpeg -rtsp_flags prefer_tcp -i "rtsp://$2:554/user=admin&password=&channel=1&stream=1.sdp" -acodec copy -vcodec copy -f hls -hls_time 30 -hls_list_size 32000 -hls_flags append_list+delete_segments -use_localtime 1 -hls_segment_filename "/home/zurikato/video-backup/$1/%Y-%m-%d_%H-%M-%S_hls.ts" /home/zurikato/video-backup/$1/playlist.m3u8
