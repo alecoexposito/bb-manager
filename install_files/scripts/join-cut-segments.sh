27ab09138564d40a4ee5369008b9bc90c3e77752
Alejandro <alecoexposito@gmail.com>
2020-08-06 17:50:07 -0400

    upload

--- install_files/scripts/join-cut-segments.sh
+++ install_files/scripts/join-cut-segments.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 folderPath=$1
 cd $folderPath
-#printf 'file %s\n' *.ts > videos.txt
-ffmpeg -safe 0 -f concat -y -i videos.txt -c copy download.mp4
-#ffmpeg -ss $startTime -t $totalSeconds -y -i videos.mp4 download.mp4
+#ffmpeg -safe 0 -f concat -y -i videos.txt -c copy download.mp4
+
+find 2*.ts | sed 's:\ :\\\ :g'| sed 's/^/file /' > fl.txt; ffmpeg -f concat -i fl.txt -c copy download.mp4; rm fl.txt
