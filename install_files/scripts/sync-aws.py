1ae63e390359e851d3af56079e5cb55220d39927
Alejandro <alecoexposito@gmail.com>
2020-08-27 15:23:09 -0400

    changes

--- install_files/scripts/sync-aws.py
+++ install_files/scripts/sync-aws.py
@@ -27,10 +27,29 @@ def updateSettingsFile():
     conn.request('GET', '/blackboxes/' + id + '/getSettings')
     resp = conn.getresponse()
     responseStr = resp.read().decode()
-    f = open('/home/zurikato/apps/tvz-media-server/settings-bb.json', 'w')
-    f.write(responseStr)
+    if is_json(responseStr):
+        f = open('/home/zurikato/apps/tvz-media-server/settings-bb.json', 'w')
+        f.write(responseStr)
+        return responseStr
+    return ''
+
+def is_json(str):
+  try:
+    json_object = json.loads(str)
+  except ValueError as e:
+    return False
+  return True
+
+
+
 path = getPath()
-updateSettingsFile()
+settingsStr = updateSettingsFile()
+settingsDict = json.loads(settingsStr)
+print(settingsDict)
+if settingsDict['deleteOnSync']:
+    print('se puede borrar en el sync')
+else
+    print('no se puede borrar en el sync')
 
 # os.system("/usr/bin/aws s3 sync s3://" + path + " /home/zurikato/apps/tvz-media-server/media --size-only --delete --exclude \"*local-*.*\"")
 # os.system("cd /home/zurikato/apps/tvz-media-server/media; /usr/bin/find $PWD -regex '.*\.\(mkv\|webm\|avi\|mp4\)$' | while read f; do /usr/bin/ffmpeg -n -i \"$f\" -ss 00:00:03 -vframes 1 -s 480x320 ${f%/*}/thumb-\"${f##*/}\".jpg; done")
