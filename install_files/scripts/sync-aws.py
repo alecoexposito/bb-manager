4bd8ebc87ad8311a188e57d403d7e3b9a935ed73
Alejandro <alecoexposito@gmail.com>
2020-08-27 17:26:07 -0400

    thumb

--- install_files/scripts/sync-aws.py
+++ install_files/scripts/sync-aws.py
@@ -61,7 +61,7 @@ else:
     os.system("/usr/bin/aws s3 sync s3://" + path + " /home/zurikato/apps/tvz-media-server/media --size-only --exclude \"*local-*.*\"")
 
 # os.system("/usr/bin/aws s3 sync s3://" + path + " /home/zurikato/apps/tvz-media-server/media --size-only --delete --exclude \"*local-*.*\"")
-# os.system("cd /home/zurikato/apps/tvz-media-server/media; /usr/bin/find $PWD -regex '.*\.\(mkv\|webm\|avi\|mp4\)$' | while read f; do /usr/bin/ffmpeg -n -i \"$f\" -ss 00:00:03 -vframes 1 -s 480x320 ${f%/*}/thumb-\"${f##*/}\".jpg; done")
+os.system("cd /home/zurikato/apps/tvz-media-server/media; /usr/bin/find $PWD -regex '.*\.\(mkv\|webm\|avi\|mp4\)$' | while read f; do /usr/bin/ffmpeg -n -i \"$f\" -ss 00:00:03 -vframes 1 -s 480x320 ${f%/*}/thumb-\"${f##*/}\".jpg; done")
 
 
 
