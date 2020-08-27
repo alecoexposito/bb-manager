b23c675b606cf9ab0e6208928cd4372b67a059a4
Alejandro <alecoexposito@gmail.com>
2020-08-27 14:18:06 -0400

    adding get settings to sync-aws script

--- install_files/scripts/sync-aws.py
+++ install_files/scripts/sync-aws.py
@@ -9,31 +9,31 @@ def loadConfiguration():
     return []
 
 def getPath():
-    print("en el metodo")
     conn = http.client.HTTPConnection('69.64.32.172',  3010)
     id = sys.argv[1]
     conn.request('GET', '/blackboxes/' + id + '/getPath')
     resp = conn.getresponse()
-    responseStr = resp.read().decode();
-    print("resp: ", responseStr)
+    responseStr = resp.read().decode()
     if resp.status == 200:
         data = json.loads(responseStr)
-        print(data)
         awsFolder = data['path']
         return awsFolder
     else:
         return ''
 
-print("llamando  al metodo")
+def updateSettingsFile():
+    conn = http.client.HTTPConnection('69.64.32.172',  3010)
+    id = sys.argv[1]
+    conn.request('GET', '/blackboxes/' + id + '/getSettings')
+    resp = conn.getresponse()
+    responseStr = resp.read().decode()
+    f = open('/home/zurikato/apps/tvz-media-server/settings-bb.json', 'w')
+    f.write(responseStr)
 path = getPath()
-print(path);
+updateSettingsFile()
 
-# /usr/local/bin/aws s3 sync s3://zurikato-dev-01/1 /home/zurikato/tmp/1 --size-only --delete
-
-os.system("/usr/bin/aws s3 sync s3://" + path + " /home/zurikato/apps/tvz-media-server/media --size-only --delete --exclude \"*local-*.*\"")
-# os.system("cd /home/zurikato/apps/tvz-media-server/media; for f in $(/usr/bin/find $PWD -regex '.*\.\(mkv\|webm\|avi\|mp4\)$'); do /usr/bin/ffmpeg -n -i $f -ss 00:00:03 -vframes 1 -s 480x320 ${f%/*}/thumb-${f##*/}.jpg; done")
-
-os.system("cd /home/zurikato/apps/tvz-media-server/media; /usr/bin/find $PWD -regex '.*\.\(mkv\|webm\|avi\|mp4\)$' | while read f; do /usr/bin/ffmpeg -n -i \"$f\" -ss 00:00:03 -vframes 1 -s 480x320 ${f%/*}/thumb-\"${f##*/}\".jpg; done")
+# os.system("/usr/bin/aws s3 sync s3://" + path + " /home/zurikato/apps/tvz-media-server/media --size-only --delete --exclude \"*local-*.*\"")
+# os.system("cd /home/zurikato/apps/tvz-media-server/media; /usr/bin/find $PWD -regex '.*\.\(mkv\|webm\|avi\|mp4\)$' | while read f; do /usr/bin/ffmpeg -n -i \"$f\" -ss 00:00:03 -vframes 1 -s 480x320 ${f%/*}/thumb-\"${f##*/}\".jpg; done")
 
 
 
