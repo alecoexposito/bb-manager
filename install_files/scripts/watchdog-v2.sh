3c221494c8e281178c6c5ed5ea04c5b11acabce9
Alejandro <alecoexposito@gmail.com>
2020-10-21 16:34:39 -0400

    changes

--- install_files/scripts/watchdog-v2.sh
+++ install_files/scripts/watchdog-v2.sh
@@ -8,11 +8,19 @@ fi
 
 echo "************ creando lock file /var/lock/bb-watchdog.lock ******************** "
 echo 0 > /var/lock/bb-watchdog.lock
-
-INFO = $(qmicli --device=/dev/cdc-wdm0 --nas-get-signal-info 2>&1)
+LTE_MIN=-90
+LTE_MAX=-50
+INFO=$(qmicli --device=/dev/cdc-wdm0 --nas-get-signal-info 2>&1)
 RSSI=$(echo $INFO | sed -n "s/^.*RSSI:\s*\(\S*\).*$/\1/p")
 
-echo $RSSI
+echo "RSSI: $RSSI"
+
+if [$RSSI < $LTE_MIN ] then
+  echo "está por debajo de $LTE_MIN, sin internet"
+else
+  echo "esta por encima de $LTE_MIN, con internet";
+fi
+
 
 
 rm $LOCK_FILE
