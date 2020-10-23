622734aae5b33615fed2b6d7758b677376f119cb
Alejandro <alecoexposito@gmail.com>
2020-10-23 11:40:10 -0400

    CHANGES

--- install_files/scripts/watchdog-v2.sh
+++ install_files/scripts/watchdog-v2.sh
@@ -12,18 +12,28 @@ do
 # echo 0 > /var/lock/bb-watchdog.lock
 LTE_MIN=-90
 LTE_MAX=-50
+echo /bin/date
+echo "gps info"
+COORDS=$(/usr/bin/qmicli -p -d /dev/cdc-wdm0 --loc-get-position-report 2>&1 | head -n 30)
+LAT=$(echo $COORDS | sed -n "s/^.*latitude:\s*\(\S*\).*$/\1/p")
+LNG=$(echo $COORDS | sed -n "s/^.*longitude:\s*\(\S*\).*$/\1/p")
+echo "$LAT, $LNG"
+
+echo "intensidad de la señal"
 INFO=$(qmicli --device=/dev/cdc-wdm0 --nas-get-signal-info 2>&1)
 RSSI=$(echo $INFO | sed -n "s/^.*RSSI:\s'*\(\S*\).*$/\1/p")
 echo "Respuesta: $INFO"
 echo "RSSI: $RSSI"
 
-/usr/bin/sleep 2
-sudo /usr/bin/ -p -d /dev/cdc-wdm0 --loc-get-position-report
+DOWN_COUNTER=0
+UP_COUNTER=0
 
 if [[ $RSSI -lt $LTE_MIN ]]; then
   echo "esta por debajo de $LTE_MIN, sin internet"
-  if [ $IS_DOWN == "False" ]; then
-    IS_DOWN="True"
+  UP_COUNTER=0
+  ((DOWN_COUNTER++))
+  if [ $DOWN_COUNTER -gt 3 ]; then
+    DOWN_COUNTER=0
     sudo /usr/bin/qmi-network /dev/cdc-wdm0 stop
     echo "corriendo ifconfig wwan0 down"
     sudo /usr/sbin/ifconfig wwan0 down
@@ -31,9 +41,11 @@ if [[ $RSSI -lt $LTE_MIN ]]; then
     /usr/bin/sleep 2
   fi
 else
-  if [ $IS_DOWN == "True" ]; then
-    echo "bajando..."
-    IS_DOWN="False"
+  DOWN_COUNTER=0
+  ((UP_COUNTER++))
+  if [ UP_COUNTER -gt 3 ]; then
+    echo "subiendo red..."
+    UP_COUNTER=0
     sudo /usr/bin/qmi-network /dev/cdc-wdm0 start
     /usr/bin/sleep 5
     sudo /usr/sbin/udhcpc -q -f -i wwan0
