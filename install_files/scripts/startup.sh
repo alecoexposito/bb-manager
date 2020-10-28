b5be14a2214b40bb3605b301ed53e522fd5b484c
Alejandro <alecoexposito@gmail.com>
2020-10-28 12:35:35 -0400

    changes

--- install_files/scripts/startup.sh
+++ install_files/scripts/startup.sh
@@ -4,8 +4,8 @@
 /usr/bin/sleep 10
 sudo /usr/bin/python /home/zurikato/scripts/at-command.py AT+QGPS=1 serial/by-id/usb-Android_Android-if02-port0 &
 /usr/bin/sleep 20
-sudo /usr/bin/qmi-network /dev/cdc-wdm0 start
-/usr/bin/sleep 5
-sudo /usr/sbin/udhcpc -q -f -i wwan0 &
-/usr/bin/sleep 45
+# sudo /usr/bin/qmi-network /dev/cdc-wdm0 start
+# /usr/bin/sleep 5
+# sudo /usr/sbin/udhcpc -q -f -i wwan0 &
+# /usr/bin/sleep 45
 sudo /usr/bin/bash /home/zurikato/scripts/watchdog-v2.sh >> /root/log/watchdog.log
